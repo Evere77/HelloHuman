b78e544076d657251903bae1595c94968c6ec440
Evere77 <116045038+Evere77@users.noreply.github.com>
2023-06-14 12:56:29 -0700

    Initial commit

---
+++ src/test/java/com/project/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
